5d83fb0753ba4c6ceeac90c7d5d5d1e14dd123d3
Roman.Zavodskikh <Roman.Zavodskikh@acronis.com>
2020-12-28 00:52:39 +0300

    Refactored regressor.predict_price

--- regressor.py
+++ regressor.py
@@ -1,4 +1,6 @@
 import joblib
+import numpy as np
+EPS = 1e-3
 
 class Regressor(object):
     def __init__(self):
@@ -6,7 +8,10 @@ class Regressor(object):
     
     def predict_price(self, in_vector):
         try:
-            return self.model.predict(in_vector)[0]
+            predicted_price = self.model.predict(in_vector)[0]
+            predicted_price = np.power(predicted_price, 2) - EPS
+            predicted_price = round(predicted_price, 2)
+            return predicted_price
         except:
             print("prediction error")
             return None 
